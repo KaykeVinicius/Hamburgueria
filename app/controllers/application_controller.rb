d022b8c9b8549e2043ebc6a56d1ba09f9bf231eb
Kayke <kaykepereira@seduc.ro.gov.br>
2024-11-08 11:39:19 -0400

    adiciona gem i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  around_action :switch_locale
+
+  private
+  
+  def default_url_options
+    { locale: I18n.locale }
+  end
+  
+def switch_locale(&action)
+  locale = params[:locale] || I18n.default_locale
+  I18n.with_locale(locale, &action)
+end
+
   allow_browser versions: :modern
 end
